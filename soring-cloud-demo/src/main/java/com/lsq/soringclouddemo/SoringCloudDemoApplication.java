1fa356ccedd08900abda207c4507e5f6bdb56579
Linshiq <284244633@qq.com>
2019-12-19 15:04:06 +0800

    初始化提交

---
+++ soring-cloud-demo/src/main/java/com/lsq/soringclouddemo/SoringCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lsq.soringclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoringCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoringCloudDemoApplication.class, args);
+	}
+
+}
---
+++ soring-cloud-demo/src/test/java/com/lsq/soringclouddemo/SoringCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsq.soringclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoringCloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
