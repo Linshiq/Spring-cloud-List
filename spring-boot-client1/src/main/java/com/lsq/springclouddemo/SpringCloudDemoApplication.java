dcb34238c0354c27dfd9bab2a85c869a44b6990d
Linshiq <284244633@qq.com>
2020-01-15 16:05:28 +0800

    初始化提交

--- spring-boot-client1/src/main/java/com/lsq/springclouddemo/SpringCloudDemoApplication.java
+++ spring-boot-client1/src/main/java/com/lsq/springclouddemo/SpringCloudDemoApplication.java
@@ -1,13 +1,13 @@
-package com.lsq.soringclouddemo;
+package com.lsq.springclouddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoringCloudDemoApplication {
+public class SpringCloudDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SoringCloudDemoApplication.class, args);
+		SpringApplication.run(SpringCloudDemoApplication.class, args);
 	}
 
 }
